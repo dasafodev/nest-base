c4fe69a9c93daf452d1ff13128e2b4f4c6b7092b
Santiago Forero <ds.foreroa@uniandes.edu.co>
2021-08-30 22:42:45 -0500

    enviroments config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+import { enviroments } from './enviroments';
+import config from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      isGlobal: true,
+      load: [config],
+      validationSchema: Joi.object({
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config.ts
@@ -0,0 +1,13 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('config', () => {
+  return {
+    database: {
+      db: process.env.POSTGRES_DB,
+      host: process.env.POSTGRES_HOST,
+      password: process.env.POSTGRES_PASSWORD,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+    },
+  };
+});
