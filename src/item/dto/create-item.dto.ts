836d999a61126326e17b611bf1a38f52dd67324a
Santiago Forero <ds.foreroa@uniandes.edu.co>
2021-08-30 23:58:17 -0500

    swagger documentation

--- src/item/dto/create-item.dto.ts
+++ src/item/dto/create-item.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmpty, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateItemDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly name: string;
 
   @IsEmpty()
+  @ApiProperty()
   readonly id: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +13,15 @@ async function bootstrap() {
   );
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
